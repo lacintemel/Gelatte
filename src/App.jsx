e49e8d54832b4a3135ed18cf5ef7667e25f191fb
Lacin Temel <lacintemel6@gmail.com>
2026-04-18 15:15:02 +0300

    feat: implement product details, checkout flow, and toast notifications with supporting styles and providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+import { ToastProvider } from './context/ToastContext';
+import { WishlistProvider } from './context/WishlistContext';
+import ScrollToTop from './components/ScrollToTop';
 import LandingPage from './pages/LandingPage';
 import ShopPage from './pages/ShopPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CheckoutPage from './pages/CheckoutPage';
 
 export default function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/shop" element={<ShopPage />} />
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <ToastProvider>
+      <WishlistProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/shop" element={<ShopPage />} />
+              <Route path="/shop/product/:id" element={<ProductDetailPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
+      </WishlistProvider>
+    </ToastProvider>
   );
 }
