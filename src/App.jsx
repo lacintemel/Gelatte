1b9b911bcf69dde9d25fb9c0b1f0ebf5ad6c33ef
Lacin Temel <lacintemel6@gmail.com>
2026-05-05 22:24:58 +0300

    feat: implement admin dashboard with product and category management, authentication, and updated price calculation logic.

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,62 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import { ToastProvider } from './context/ToastContext';
 import { WishlistProvider } from './context/WishlistContext';
+import { LanguageProvider } from './context/LanguageContext';
+import { AuthProvider } from './context/AuthContext';
+import { ProductProvider } from './context/ProductContext';
+
 import ScrollToTop from './components/ScrollToTop';
 import LandingPage from './pages/LandingPage';
 import ShopPage from './pages/ShopPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CheckoutPage from './pages/CheckoutPage';
 
-import { LanguageProvider } from './context/LanguageContext';
+// Admin Components
+import AdminProtectedRoute from './components/admin/AdminProtectedRoute';
+import AdminLayout from './layouts/AdminLayout';
+import AdminLogin from './pages/admin/AdminLogin';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import AdminProducts from './pages/admin/AdminProducts';
+import AdminProductForm from './pages/admin/AdminProductForm';
+import AdminCategories from './pages/admin/AdminCategories';
 
 export default function App() {
   return (
-    <LanguageProvider>
-      <ToastProvider>
-        <WishlistProvider>
-          <CartProvider>
-            <BrowserRouter>
-              <ScrollToTop />
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/shop" element={<ShopPage />} />
-                <Route path="/shop/product/:id" element={<ProductDetailPage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-              </Routes>
-            </BrowserRouter>
-          </CartProvider>
-        </WishlistProvider>
-      </ToastProvider>
-    </LanguageProvider>
+    <AuthProvider>
+      <ProductProvider>
+        <LanguageProvider>
+          <ToastProvider>
+            <WishlistProvider>
+              <CartProvider>
+                <BrowserRouter>
+                  <ScrollToTop />
+                  <Routes>
+                    {/* Public Routes */}
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/shop" element={<ShopPage />} />
+                    <Route path="/shop/product/:id" element={<ProductDetailPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+
+                    {/* Admin Login */}
+                    <Route path="/admin/login" element={<AdminLogin />} />
+
+                    {/* Protected Admin Routes */}
+                    <Route path="/admin" element={<AdminProtectedRoute />}>
+                      <Route element={<AdminLayout />}>
+                        <Route index element={<AdminDashboard />} />
+                        <Route path="products" element={<AdminProducts />} />
+                        <Route path="products/new" element={<AdminProductForm />} />
+                        <Route path="products/:id" element={<AdminProductForm />} />
+                        <Route path="categories" element={<AdminCategories />} />
+                      </Route>
+                    </Route>
+                  </Routes>
+                </BrowserRouter>
+              </CartProvider>
+            </WishlistProvider>
+          </ToastProvider>
+        </LanguageProvider>
+      </ProductProvider>
+    </AuthProvider>
   );
 }
